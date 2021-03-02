c86e6399120d79d8c4b524c5d1a498933ae7444b
Midii-11 <jak.daxter.midii@gmail.com>
2021-03-02 02:45:57 +0100

    Preprocessing Creators

--- Preprocessing.py
+++ Preprocessing.py
@@ -14,8 +14,9 @@ def goal_Overall_score(df_interest, DISPLAY):
             else:
                 df_interest.iloc[index, 1] = np.nan
         finally:
-            print(index, '\t', df_interest.iloc[index, 1])
-    print(df_interest.shape)
+            # print(index, '\t', df_interest.iloc[index, 1])
+            pass
+    print('\n Preprocessed shape:\t', df_interest.shape)
 
     # Show Histogram of the Overall_scores
     if DISPLAY is True:
@@ -28,15 +29,46 @@ def goal_Overall_score(df_interest, DISPLAY):
     return df_interest
 
 
+def goalCreators(df_interest):
+    # Displays a pie chart representation of the creators
+    if DISPLAY is True:
+        creators = df_interest.creator.value_counts().to_frame().T
+        # print(df_interest.creator.value_counts().to_frame().T)
+        plot.subplots(figsize=(10, 8))
+        patches, texts = plot.pie(x=creators.values[0], startangle=90)
+        plot.legend(patches, list(creators), loc="best", bbox_to_anchor=(1, 1))
+        plot.title('Pie chart: Creators')
+        plot.show()
+
+    # Replace smaller creators by 'other' -> 3 categories
+    for index in df_interest.index:
+        if df_interest.iloc[index, 5] != 'Marvel Comics' and df_interest.iloc[index, 5] != 'DC Comics':
+            df_interest.iloc[index, 5] = 'Other'
+
+    # Displays a pie chart representation of the creators (Marvel / DC / Other)
+    if DISPLAY is True:
+        creators = df_interest.creator.value_counts().to_frame().T
+        # print(df_interest.creator.value_counts().to_frame().T)
+        plot.subplots(figsize=(10, 8))
+        patches, texts = plot.pie(x=creators.values[0], startangle=90)
+        plot.legend(patches, list(creators), loc="best", bbox_to_anchor=(1, 1))
+        plot.title('Pie chart: Creators (Gathered)')
+        plot.show()
+
+    print(df_interest.creator)
+    return df_interest
 
 
 
 if __name__ == '__main__':
 
     # Displays graphs
-    DISPLAY = True
+    # DISPLAY = True
+    DISPLAY = False
+
     # Saves DF
-    SAVE = True
+    # SAVE = True
+    SAVE = False
 
     # Load data into a pandas dataframe
     df_full = pd.read_csv("datasets/superheroes_nlp_dataset.csv")
@@ -44,16 +76,22 @@ if __name__ == '__main__':
                                   "superpowers", "creator", "alignment")]
 
     # Get the number of null entries in the interesting data
+    print('\n Initial shape:\t', df_interest.shape)
     print(df_interest.isnull().sum())
-    print(df_interest.shape)
 
     # Remove rows containing empty values in main categories
     df_interest.dropna(subset=["history_text", "powers_text"], inplace=True)
     df_interest.reset_index(drop=True, inplace=True)
 
-    # Create the dataset for Overall_score prediction from history and power txts
+    # -----------------------------------
+    # Calls functions
+    # -----------------------------------
+    # Create the dataset for Overall_score prediction from history and power texts
     df_interest = goal_Overall_score(df_interest, DISPLAY)
+    # Create the dataset for Creator prediction from history and power texts
+    df_interest = goalCreators(df_interest)
 
     # Save the preprocessed dataset
     if SAVE is True:
-        df_interest.to_csv("datasets/Preprocessed.csv")
+        df_interest.to_csv("datasets/Preprocessed.csv", index=False)
+        print('\n Dataset SAVED.')
